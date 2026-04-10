7ced20381c423e12526855eb7dfe0c9d88eabea9
momak_bb <freeboy_bb@yahoo.co.uk>
2026-04-10 08:33:31 +0200

    blazor notFound page

--- bateka.games/App.razor.cs
+++ bateka.games/App.razor.cs
@@ -1,20 +1,35 @@
 ﻿using Microsoft.AspNetCore.Components;
 using Microsoft.AspNetCore.Components.Web;
+using Microsoft.JSInterop;
 
 namespace bateka.games;
 
-public partial class App
+public partial class App : ComponentBase
 {
     [Inject]
     private NavigationManager NavigationManager { get; set; } = null!;
+    [Inject] private IJSRuntime JS { get; set; } = default!;
 
     private ErrorBoundary? _errorBoundary;
+
+    protected override async Task OnAfterRenderAsync(bool firstRender)
+    {
+        if (firstRender)
+        {
+            var path = await JS.InvokeAsync<string?>("eval", "window.__spaRedirectPath");
+            if (!string.IsNullOrEmpty(path))
+            {
+                await JS.InvokeVoidAsync("eval", "window.__spaRedirectPath = null");
+                NavigationManager.NavigateTo(path, replace: true);
+            }
+        }
+    }
 
     private void RecoverFromError() => _errorBoundary?.Recover();
 
     private void ToHome()
     {
         _errorBoundary?.Recover();
-        NavigationManager.NavigateTo("Lists");
+        NavigationManager.NavigateTo("");
     }
 }
