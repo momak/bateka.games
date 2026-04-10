403e7589e756c036dca21f719533083237c721c1
momak_bb <freeboy_bb@yahoo.co.uk>
2026-04-10 08:15:32 +0200

    home page navigation

--- bateka.games/Layout/NavMenu.razor.cs
+++ bateka.games/Layout/NavMenu.razor.cs
@@ -1,5 +1,8 @@
-﻿namespace bateka.games.Layout;
+﻿using Microsoft.AspNetCore.Components;
+
+namespace bateka.games.Layout;
 
 public partial class NavMenu
 {
+    [Inject] private NavigationManager Nav { get; set; } = default!;
 }
