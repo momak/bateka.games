e855d0b244404ca433159464afab8f984eeed88d
momak <freeboy_bb@yahoo.co.uk>
2026-04-10 05:21:04 +0000

    Deploying to gh-pages from @ momak/bateka.games@d853dfa722a25a35ccb5272e6e6f224ccedbfacc 🚀

--- service-worker.js
+++ service-worker.js
@@ -1,4 +1,4 @@
-/* Manifest version: 9nzdaFyi */
+/* Manifest version: w2h49W42 */
 // Caution! Be sure you understand the caveats before publishing an application with
 // offline support. See https://aka.ms/blazor-offline-considerations
 
